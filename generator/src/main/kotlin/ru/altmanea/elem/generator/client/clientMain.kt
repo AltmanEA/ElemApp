f5e4922d82a6fa82aa0420048074e5748b561cc6
AltmanEvgenii <AltmanEA@gmail.com>
2022-07-02 18:56:39 +0600

    nav by react route

--- generator/src/main/kotlin/ru/altmanea/elem/generator/client/clientMain.kt
+++ generator/src/main/kotlin/ru/altmanea/elem/generator/client/clientMain.kt
@@ -9,7 +9,13 @@ val reactFC = ClassName("react", "FC")
 val reactProps = ClassName("react", "Props")
 val reactCreate = MemberName("react", "create")
 val reactCreateRoot = MemberName("react.dom.client", "createRoot")
-val reactH1 = MemberName("react.dom.html.ReactHTML","h1")
+
+val reactH1 = MemberName("react.dom.html.ReactHTML", "h1")
+val reactUl = MemberName("react.dom.html.ReactHTML", "ul")
+val reactLi = MemberName("react.dom.html.ReactHTML", "li")
+val reactNav = MemberName("react.router.dom", "NavLink")
+val reactHashRouter = MemberName("react.router.dom", "HashRouter")
+
 
 //public fun main(): Unit {
 //    val container = document.createElement("div")
@@ -31,21 +37,45 @@ fun Generator.clientMain(): FileSpec {
     val fileSpec = FileSpec
         .builder(config.packageName, "${config.name}ClientMain")
 
+    val navCode = CodeBlock
+        .builder()
+        .beginControlFlow("%T<%T>", reactFC, reactProps)
+        .beginControlFlow("%M", reactUl)
+    config.elems.forEach {
+        navCode
+            .beginControlFlow("%M", reactLi)
+            .beginControlFlow("%M", reactNav)
+            .addStatement("to = %S", it.name)
+            .addStatement("+%S", it.name)
+            .endControlFlow()
+            .endControlFlow()
+    }
+    navCode
+        .endControlFlow()
+        .endControlFlow()
+
+    val navProp =
+        PropertySpec
+            .builder("Nav", reactFC.parameterizedBy(reactProps))
+            .initializer(navCode.build())
+            .build()
+
     val appCode =
         CodeBlock
             .builder()
             .beginControlFlow("%T<%T>", reactFC, reactProps)
-            .beginControlFlow("%M", reactH1)
-            .addStatement("+%S", "Hello")
+            .beginControlFlow("%M", reactHashRouter)
+            .addStatement("%N {}", navProp)
             .endControlFlow()
             .endControlFlow()
-            .build()
 
     val appProp =
         PropertySpec
             .builder("App", reactFC.parameterizedBy(reactProps))
-            .initializer(appCode)
+            .initializer(appCode.build())
             .build()
+
+
 
     val mainFun = FunSpec
         .builder("main")
@@ -58,6 +88,7 @@ fun Generator.clientMain(): FileSpec {
     return fileSpec
         .addFunction(mainFun)
         .addProperty(appProp)
+        .addProperty(navProp)
         .build()
 }
 
